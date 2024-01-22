e4dfc1d23681b32434995a1bd94a8a4551f4d66a
Minato <erms12.99@hotmail.com>
2024-01-22 11:09:44 -0600

    Proyecto curso-alumno-angular-ts

---
+++ src/app/components/tabla-alumnos/mock.ts
@@ -0,0 +1,17 @@
+export const alumnos = [
+  {
+    nombre: 'Ricardo',
+    apellido: 'Perez',
+    promedio: 8.5,
+  },
+  {
+    nombre: 'Florencia',
+    apellido: 'Gonzales',
+    promedio: 9.5,
+  },
+  {
+    nombre: 'Pedro',
+    apellido: 'Francella',
+    promedio: 6,
+  },
+];
---
+++ src/app/components/tabla-cursos/mock.ts
@@ -0,0 +1,22 @@
+export const cursos = [
+  {
+    materia: 'Javascript',
+    dificultad: 6,
+    requiere: 'HTML y CSS',
+  },
+  {
+    materia: 'Typescript',
+    dificultad: 7,
+    requiere: 'Javascript',
+  },
+  {
+    materia: 'Angular',
+    dificultad: 8,
+    requiere: 'HTML, CSS, JS, TS',
+  },
+  {
+    materia: 'React',
+    dificultad: 9,
+    requiere: 'HTML, CSS, JS, TS',
+  },
+];
